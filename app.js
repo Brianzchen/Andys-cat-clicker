b259464f969c2763f18c7defb6c87a3f410a6676
Brian Chen <bchen.chen93@gmail.com>
2015-07-14 16:12:20 +1200

    removed unnecessary code from old tutorials

--- app.js
+++ app.js
@@ -47,59 +47,10 @@ var model = {
 		"currentCat": 0
 };
 
-var catList = {
-	render: function() {
-		for (i = 0; i < model.cats.length; i++) {
-			var currentCat = model.cats[i];
-			var button = '<button id="' + currentCat.list + '" class="buttons">' + currentCat.name + '</button>';
-			$("#catList").append(button);
-		}
-	}
-
-};
-
-var catImage = {
-	render: function() {
-		var catSpace = $("#catImage");
-		catSpace.empty();
-		var catId = model.currentCat;
-		var image = '<img class="cat" src="' + model.cats[catId].image + '"></img>';
-		catSpace.append(image);
-	}
-};
-
-var catImageClicks = {
-	render: function() {
-		var catSpace = $("#catImageClicks");
-		catSpace.empty();
-		var catId = model.currentCat;
-		var timesClicked = model.cats[catId].click;
-		var formattedTimesClicked = "<p>" + timesClicked + "</p>";
-		catSpace.append(formattedTimesClicked);
-	}
-};
-
 var myViewModel = {
-	init: function() {
-		catList.render();
-		catImage.render();
-		catImageClicks.render();
-		for (i = 0; i < model.cats[model.currentCat].nickName.length; i++) {
-			this.catNickname.push(model.cats[model.currentCat].nickName[i]);
-		}
-	},
-	click: function(data) {
-		if (data.toElement.className === "cat") {
-			model.cats[model.currentCat].click++;
-			catImageClicks.render();
-		}
-	},
-	buttonPress: function(data) {
-		model.currentCat = data.toElement.id;
-		catImage.render();
-		catImageClicks.render();
-	},
 	catAge: ko.observable(model.cats[model.currentCat].click),
+	catType: ko.observable(model.cats[model.currentCat].type),
+	catNickname: ko.observableArray(model.cats[model.currentCat].nickName),
 	clicker: function() {
 		this.catAge(this.catAge()+1);
 		if (this.catAge() > 5) {
@@ -107,17 +58,7 @@ var myViewModel = {
 		} else {
 			this.catType("newborn");
 		}
-	},
-	catType: ko.observable(model.cats[model.currentCat].type),
-	catNickname: ko.observableArray()
+	}
 };
-
-myViewModel.init();
-$("#catImage").click(function(data) {
-	myViewModel.click(data);
-});
-$(".buttons").click(function(data) {
-	myViewModel.buttonPress(data);
-});
 
 ko.applyBindings(myViewModel);
